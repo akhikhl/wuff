f8958552e5cc43fa46d95603931cbaf361534ebc
Andrey Hihlovskiy <akhikhl@gmail.com>
2014-04-11 22:36:04 +0200

    fixed imports in ide-bundle-1

--- examples/ide-bundle-1/src/main/java/idebundle1/MenuHandler.java
+++ examples/ide-bundle-1/src/main/java/idebundle1/MenuHandler.java
@@ -3,6 +3,7 @@ package idebundle1;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.PlatformUI;
 
 public final class MenuHandler extends AbstractHandler {
