5c6c712cac50d3ca152a06b1c4b1da293d3a19df
Andrey Hihlovskiy <akhikhl@gmail.com>
2014-04-21 15:17:14 +0200

    added tutorialExamples/RcpApp-2

---
+++ tutorialExamples/RcpApp-2/MyRcpApp/src/main/java/myrcpapp/Perspective.java
@@ -0,0 +1,12 @@
+package myrcpapp;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+		layout.setEditorAreaVisible(false);
+		layout.setFixed(true);
+	}
+}
---
+++ tutorialExamples/RcpApp-2/README.md
@@ -0,0 +1,1 @@
+This example corresponds to steps 1-5 of [RCP application tutorial](https://github.com/akhikhl/wuff/wiki/Eclipse-RCP-application).
