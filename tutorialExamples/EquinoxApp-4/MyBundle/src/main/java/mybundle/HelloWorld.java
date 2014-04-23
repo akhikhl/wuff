7a6d75a4ff775cbc70b18f1837398eba2f7986c4
Andrey Hihlovskiy <akhikhl@gmail.com>
2014-04-23 20:33:15 +0200

    updated message in EquinoxApp-4

--- tutorialExamples/EquinoxApp-4/MyBundle/src/main/java/mybundle/HelloWorld.java
+++ tutorialExamples/EquinoxApp-4/MyBundle/src/main/java/mybundle/HelloWorld.java
@@ -3,6 +3,6 @@ package mybundle;
 public class HelloWorld {
 
   public static void sayHello() {
-    System.out.println("Hello, world!");
+    System.out.println("Hello, world! I am OSGi bundle!");
   }
 }
