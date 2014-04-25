603bf81161a2860744112c944cf9ba2a69d84bf7
Andrey Hihlovskiy <akhikhl@gmail.com>
2014-04-25 20:56:51 +0200

    added tutorialExamples/PluginXml-2

---
+++ tutorialExamples/PluginXml-2/MyEclipsePlugin/src/main/java/myeclipseplugin/MyPerspective.java
@@ -0,0 +1,10 @@
+package myeclipseplugin;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class MyPerspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+	}
+}
---
+++ tutorialExamples/PluginXml-2/MyEclipsePlugin/src/main/java/myeclipseplugin/MyView.java
@@ -0,0 +1,15 @@
+package myeclipseplugin;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class MyView extends ViewPart {
+
+  @Override
+  public void createPartControl(final Composite parent) {
+  }
+
+  @Override
+  public void setFocus() {
+  }
+}
