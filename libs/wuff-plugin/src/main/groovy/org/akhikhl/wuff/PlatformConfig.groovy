7c5abb32442874e0235703207db521dc1dbe750d
Andrey Hihlovskiy <akhikhl@gmail.com>
2014-06-22 12:54:33 +0200

    fixed macosx support in PlatformConfig

--- libs/wuff-plugin/src/main/groovy/org/akhikhl/wuff/PlatformConfig.groovy
+++ libs/wuff-plugin/src/main/groovy/org/akhikhl/wuff/PlatformConfig.groovy
@@ -48,6 +48,8 @@ class PlatformConfig {
       current_os = 'linux'
     else if(current_os.substring(0, 7).equalsIgnoreCase('windows'))
       current_os = 'windows'
+    else if(current_os.equalsIgnoreCase('Mac OS X'))
+      current_os = 'macosx'
 
     current_arch = System.getProperty('os.arch')
     if(current_arch == 'x86' || current_arch == 'i386')
