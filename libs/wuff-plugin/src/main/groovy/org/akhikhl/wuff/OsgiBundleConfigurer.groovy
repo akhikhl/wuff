f3b2fffc3fd9bf8a6fbcc88a37b75c17c0fe9b11
Andrey Hihlovskiy <akhikhl@gmail.com>
2014-07-24 08:12:57 +0200

    replaced printf to project.logger.warn in OsgiBundleConfigurer

--- libs/wuff-plugin/src/main/groovy/org/akhikhl/wuff/OsgiBundleConfigurer.groovy
+++ libs/wuff-plugin/src/main/groovy/org/akhikhl/wuff/OsgiBundleConfigurer.groovy
@@ -14,6 +14,7 @@ import org.apache.commons.lang3.StringEscapeUtils
 import org.gradle.api.Project
 import org.gradle.api.java.archives.Manifest
 import org.gradle.api.tasks.bundling.Jar
+import org.slf4j
 
 /**
  *
@@ -53,7 +54,7 @@ class OsgiBundleConfigurer extends JavaConfigurer {
         else if(bundleName.toString().startsWith("org.osgi") || bundleName.toString().startsWith("org.eclipse"))
           project.dependencies.add 'compile', "${project.ext.eclipseMavenGroup}:$bundleName:+"
         else
-          printf("Warning: Bundle name %s could not be found\n", bundleName)
+          project.logger.warn 'Bundle name {} could not be found', bundleName
       }
     }
     userManifest?.mainAttributes?.getValue('Require-Bundle')?.split(',')?.each { bundle ->
