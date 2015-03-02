efd94429b37757299affb0888177712b7f5423e8
Michał Ćmil <cmilmichal@gmail.com>
2015-03-02 23:09:40 +0100

    Introduction of Efxclipse grade plugin with E4 application support

---
+++ libs/wuff-plugin/src/main/groovy/org/akhikhl/wuff/EfxclipseAppConfigurer.groovy
@@ -0,0 +1,18 @@
+/*
+ * wuff
+ *
+ * Copyright 2014  Andrey Hihlovskiy.
+ *
+ * See the file "LICENSE" for copying and usage permission.
+ */
+package org.akhikhl.wuff
+
+import org.gradle.api.Project
+
+class EfxclipseAppConfigurer extends EquinoxAppConfigurer {
+
+  EfxclipseAppConfigurer(Project project) {
+    super(project)
+  }
+}
+
