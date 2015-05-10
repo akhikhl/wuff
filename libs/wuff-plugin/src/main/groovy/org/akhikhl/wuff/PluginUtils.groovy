f392c36fdefcdb5b42acb5cea747769d62a5586f
Yann Le Moigne <ylemoigne@javatic.fr>
2015-05-11 00:11:07 +0200

    Externalize some configuration elements (task name, outputdir) in PluginUtils

--- libs/wuff-plugin/src/main/groovy/org/akhikhl/wuff/PluginUtils.groovy
+++ libs/wuff-plugin/src/main/groovy/org/akhikhl/wuff/PluginUtils.groovy
@@ -7,6 +7,8 @@
  */
 package org.akhikhl.wuff
 
+import org.akhikhl.unpuzzle.PlatformConfig
+
 import java.nio.file.Paths
 import java.util.Properties
 import java.util.jar.Attributes
@@ -332,4 +334,48 @@ final class PluginUtils {
     new File(project.buildDir, 'tmp-osgi/MANIFEST.MF')
   }
 
+  static String getProductTaskSuffix(Map product) {
+    String platform = product.platform ?: PlatformConfig.current_os
+    String arch = product.arch ?: PlatformConfig.current_arch
+    String language = product.language ?: ''
+
+    if(product.taskSuffix)
+      product.taskSuffix
+    else {
+      String productNamePrefix = product.name ? "${product.name}_" : ''
+      String languageSuffix = language ? "_${language}" : ''
+      "${productNamePrefix}${platform}_${arch}${languageSuffix}"
+    }
+  }
+
+  static String getProductBuildTaskName(Map product){
+    "buildProduct_${getProductTaskSuffix(product)}"
+  }
+
+  static String getArchiveProductTaskName(Map product){
+    "archiveProduct_${getProductTaskSuffix(product)}"
+  }
+
+  static String getProductFileSuffix(Map product){
+    String platform = product.platform ?: PlatformConfig.current_os
+    String arch = product.arch ?: PlatformConfig.current_arch
+    String language = product.language ?: ''
+
+    if(product.fileSuffix)
+      product.fileSuffix
+    else {
+      String productNamePrefix = product.name ? "${product.name}-" : ''
+      String languageSuffix = language ? "-${language}" : ''
+      "${productNamePrefix}${platform}-${arch}${languageSuffix}"
+    }
+  }
+
+  static File getProductOutputDir(Project project, Map product){
+    String productOutputDirName = "${project.name}-${project.version}"
+    String productFileSuffix = getProductFileSuffix(product)
+    if(productFileSuffix)
+      productOutputDirName += '-' + productFileSuffix
+
+    new File(getProductOutputBaseDir(project), productOutputDirName)
+  }
 }
